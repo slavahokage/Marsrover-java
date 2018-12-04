085ac17ec9466a4abfb36316492936f95e735775
Vyacheslav Smirnov <v_smirnov@ikantam.com>
2018-12-04 12:20:15 +0300

    add some tests

--- src/test/java/TestResults.java
+++ src/test/java/TestResults.java
@@ -6,10 +6,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestResults {
 
     @Test
-    public void test() {
+    public void test1() {
         MarsRover marsRover = new MarsRover();
         assertEquals(13, marsRover.getMinCountOfInstructions(26));
+    }
+
+    @Test
+    public void test2() {
+        MarsRover marsRover = new MarsRover();
         assertEquals(2, marsRover.getMinCountOfInstructions(3));
+    }
+
+
+    @Test
+    public void test3() {
+        MarsRover marsRover = new MarsRover();
         assertEquals(7, marsRover.getMinCountOfInstructions(5));
+    }
+
+    @Test
+    public void test4() {
+        MarsRover marsRover = new MarsRover();
+        assertEquals(5, marsRover.getMinCountOfInstructions(6));
     }
 }
